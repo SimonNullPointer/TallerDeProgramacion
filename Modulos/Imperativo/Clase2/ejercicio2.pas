5974e9ce04dba61c1d5f6415d5a60a531e68f022
SimonNullPointer <simoncastillo161803@gmail.com>
2024-08-23 16:56:51 -0300

    ejercicio 2 de recursion como un puto toroooo

---
+++ Modulos/Imperativo/Clase2/ejercicio2.pas
@@ -0,0 +1,135 @@
+
+Program ejercicio2;
+
+Const 
+  min = 100;
+  max = 200;
+
+Type 
+  lista = ^nodo;
+  nodo = Record
+    dato: Integer;
+    sig: lista;
+  End;
+
+Procedure LlenarListaRecursiva(Var pri,ult:lista);
+Procedure generarLista(Var l:lista);
+Begin
+  l := Nil;
+End;
+
+Procedure AgregarElemento(Var pri,ult:lista; n:Integer);
+
+Var 
+  aux: lista;
+Begin
+  new(aux);
+  aux^.dato := n;
+  aux^.sig := Nil;
+  If (pri = Nil) Then
+    Begin
+      pri := aux;
+      ult := aux;
+    End
+  Else
+    Begin
+      ult^.sig := aux;
+      ult := aux;
+    End;
+End;
+
+Procedure LlenarLista(Var pri,ult:lista);
+
+Var 
+  num : Integer;
+Begin
+  num := min + random(max-min+1);
+  If (num <> min) Then
+    Begin
+      AgregarElemento(pri,ult,num);
+      LlenarLista(pri,ult);
+      // recursividad
+    End;
+End;
+
+Begin
+  generarLista(pri);
+  LlenarLista(pri,ult)
+End;
+
+Procedure ImprimirListaRecursiva(l:lista);
+Begin
+  If (l<>Nil) Then
+    Begin
+      Write(l^.dato);
+      Write(' | ');
+      ImprimirListaRecursiva(l^.sig);
+    End;
+End;
+
+Procedure ImprimirListaInversaRecursiva(l:lista);
+Begin
+  If (l <> Nil) Then
+    Begin
+      ImprimirListaInversaRecursiva(l^.sig);
+      Write(l^.dato);
+      Write(' | ');
+    End;
+End;
+
+Function CalcularMinimoRecursivo(l:lista): Integer;
+
+Var 
+  Minimo: Integer;
+Begin
+  If l=Nil Then
+    Minimo := 999
+  Else
+    Begin
+      Minimo := CalcularMinimoRecursivo(l^.sig);
+      If (l^.dato<Minimo) Then
+        CalcularMinimoRecursivo := l^.dato
+      Else
+        CalcularMinimoRecursivo := Minimo
+    End;
+End;
+
+
+Function existe(l:lista; n:Integer): Boolean;
+Begin
+  If (l = Nil) Then
+    existe := False
+  Else If (l^.dato = n) Then
+         existe := True
+  Else
+    existe := existe(l^.sig, n);
+End;
+
+
+Var 
+  pri,ult: lista;
+  n: Integer;
+Begin
+  randomize;
+  LlenarListaRecursiva(pri,ult);
+
+  WriteLn('Lista impresa en orden:');
+  ImprimirListaRecursiva(pri);
+
+  WriteLn();
+  WriteLn();
+
+  WriteLn('Lista impresa al reves:');
+  ImprimirListaInversaRecursiva(pri);
+
+  WriteLn();
+  WriteLn();
+
+  WriteLn('El numero mas bajo de la lista es:');
+  WriteLn(CalcularMinimoRecursivo(pri));
+  WriteLn();
+  WriteLn();
+  WriteLn('Ingrese valor a buscar:');
+  ReadLn(n);
+  WriteLn(existe(pri,n));
+End.
